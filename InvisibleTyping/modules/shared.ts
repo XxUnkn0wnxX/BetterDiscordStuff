e8c38e9fb0f54d94f8f8c9b9fb81d1380870556a
Dominik <me@domi-btnr.dev>
2026-04-26 15:52:27 +0200

    InvisibleTyping: 🩹 Fix Patches
    Fixes #495 #491

---
+++ InvisibleTyping/modules/shared.ts
@@ -0,0 +1,29 @@
+import { Webpack } from "@api";
+import { Flux as FluxType, FluxDispatcher } from "@vencord/discord-types";
+
+import { ClassValue } from "../types";
+
+export const Dispatcher: FluxDispatcher = Webpack.getByKeys("dispatch", "register", { searchExports: true });
+export const Flux: FluxType = Webpack.getByKeys("Store");
+export const TypingModule: { startTyping: Function, stopTyping: Function } = Webpack.getByKeys("startTyping");
+
+export const buildClassName = (...args: ClassValue[]): string => {
+    return args.reduce<string[]>((classNames, arg) => {
+        if (!arg) return classNames;
+
+        if (typeof arg === "string" || typeof arg === "number") {
+            classNames.push(String(arg));
+        } else if (Array.isArray(arg)) {
+            const nestedClassNames = buildClassName(...arg);
+            if (nestedClassNames) classNames.push(nestedClassNames);
+        } else if (typeof arg === "object") {
+            for (const key in arg) {
+                if (Object.prototype.hasOwnProperty.call(arg, key) && arg[key]) {
+                    classNames.push(key);
+                }
+            }
+        }
+
+        return classNames;
+    }, []).join(" ");
+};
---
+++ InvisibleTyping/types/index.ts
@@ -0,0 +1,85 @@
+import type { Channel } from "@vencord/discord-types";
+
+type ChatButtonsProps = {
+    type: {
+        analyticsName: string;
+        attachments: boolean;
+        autocomplete: {
+            addReactionShortcut: boolean;
+            forceChatLayer: boolean;
+            reactions: boolean;
+        };
+        commands: {
+            enabled: boolean;
+        };
+        confetti: {
+            button: boolean;
+        };
+        drafts: {
+            type: number;
+            commandType: number;
+            autoSave: boolean;
+        };
+        emojis: {
+            button: boolean;
+        };
+        gifs: {
+            button: boolean;
+            allowSending: boolean;
+        };
+        gifts: {
+            button: boolean;
+        };
+        permissions: {
+            requireSendMessages: boolean;
+        };
+        showThreadPromptOnReply: boolean;
+        stickers: {
+            button: boolean;
+            allowSending: boolean;
+            autoSuggest: boolean;
+        };
+        soundmoji: {
+            allowSending: boolean;
+        };
+        users: {
+            allowMentioning: boolean;
+        };
+        submit: {
+            button: boolean;
+            ignorePreference: boolean;
+            disableEnterToSubmit: boolean;
+            clearOnSubmit: boolean;
+            useDisabledStylesOnSubmit: boolean;
+        };
+        uploadLongMessages: boolean;
+        upsellLongMessages: {
+            iconOnly: boolean;
+        };
+        showCharacterCount: boolean;
+        sedReplace: boolean;
+        showSlowmodeIndicator: boolean;
+        showTypingIndicator: boolean;
+    };
+    disabled: boolean;
+    channel: Channel;
+    isEmpty: boolean;
+    showAllButtons: boolean;
+    textValue?: string;
+};
+
+export type ChatButtonsArgs = [ChatButtonsProps, any];
+
+export type ClassValue =
+    | string
+    | number
+    | null
+    | undefined
+    | ClassDictionary
+    | ClassArray;
+
+interface ClassDictionary {
+    [key: string]: any;
+}
+
+interface ClassArray extends Array<ClassValue> {}
